11fd0d9c09f54bbc47fe03a9ecbfc938a8af6ac7
gtandu <tbrandon91@hotmail.fr>
2023-03-26 19:02:20 +0200

    Ajout home page + cover avec progress bar

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -11,8 +14,11 @@ import { HomeComponent } from './home/home.component';
     exports: [
         HomeComponent
     ],
-    imports: [
-        CommonModule
-    ]
+  imports: [
+    CommonModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatIconModule
+  ]
 })
 export class HomeModule { }
